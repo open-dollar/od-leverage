5751f273b1403229e2e39079f21d6a028e177111
Hunter King <hking6776@gmail.com>
2024-06-11 17:59:49 -0600

    cleantree

---
+++ src/AlgebraFlash.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import "@algebra-core/interfaces/callback/IAlgebraFlashCallback.sol";
+import "@algebra-core/libraries/LowGasSafeMath.sol";
+
+import "@algebra-periphery/base/PeripheryPayments.sol";
+import "@algebra-periphery/base/PeripheryImmutableState.sol";
+import "@algebra-periphery/libraries/PoolAddress.sol";
+import "@algebra-periphery/libraries/CallbackValidation.sol";
+import "@algebra-periphery/libraries/TransferHelper.sol";
+import "@algebra-periphery/interfaces/ISwapRouter.sol";
+
+contract AlgebraFlash {}
